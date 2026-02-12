e28f090fa23216f6eab9d1827166b66727e1dd68
Linkthero <amsdrgath@gmail.com>
2026-02-12 08:54:04 +0100

    Se arregla el error

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,15 +12,15 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //if (Input.GetButtonDown("Fire1"))
-        //{
-        //    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); // Create a ray from the camera to the mouse position
-        //    RaycastHit hit = new RaycastHit(); // Create a RaycastHit object to store information about what was hit
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); // Create a ray from the camera to the mouse position
+            RaycastHit hit = new RaycastHit(); // Create a RaycastHit object to store information about what was hit
 
-        //    if (Physics.Raycast(ray, out hit, 1000))     // Cast a ray from the camera to the mouse position
-        //    {
-        //        GetComponent<NavMeshAgent>().SetDestination(hit.point); // Set the destination of the NavMeshAgent to the point that was hit
-        //    }
-        //}
+            if (Physics.Raycast(ray, out hit, 1000))     // Cast a ray from the camera to the mouse position
+            {
+                GetComponent<NavMeshAgent>().SetDestination(hit.point); // Set the destination of the NavMeshAgent to the point that was hit
+            }
+        }
     }
 }
